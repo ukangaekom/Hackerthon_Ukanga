3f866218556e54f5f8beda53369c79b1fb413cc9
Coin-boy <ekomabasiuk@gmail.com>
2024-05-10 14:45:01 +0100

    work going on

---
+++ obot_revamp_frontend/src/app/page.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>This is Obot Revamp Project</h1>
+    </main>
+  );
+}
+
+
+
+
+// MAIN COMPONENTS
+
+const navBar = () => {
+  
+}
+
+const viewPage = () => {
+  
+}
+
+const reportBoard = () => {
+  
+}
+
+const searchBoard = () => {
+  
+}
+
+const dataBoard = () => {
+  
+}
+
+
+// SUBMAIN COMPONENTS
+
+const contractComponent = () => {
+  
+}
